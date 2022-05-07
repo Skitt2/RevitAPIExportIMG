e2f54dcb3829a19c5f56b2a85f087af609386656
Skitt2 <provaid@yandex.ru>
2022-05-07 15:25:39 +0300

    Add project files.

---
+++ RevitAPIExportIMG/Main.cs
@@ -0,0 +1,35 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPIExportIMG
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            ImageExportOptions imgExportOptions = new ImageExportOptions
+            {
+                ZoomType = ZoomFitType.FitToPage,
+                PixelSize = 1024,
+                FilePath = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\123img.jpeg",
+                FitDirection = FitDirectionType.Horizontal,
+                HLRandWFViewsFileType = ImageFileType.JPEGLossless,
+                ImageResolution = ImageResolution.DPI_600,
+                ExportRange = ExportRange.CurrentView,
+            };
+            doc.ExportImage(imgExportOptions);
+
+            TaskDialog.Show("Экспорт", "Экспорт прошел удачно");
+            return Result.Succeeded;
+        }
+    }
+}
